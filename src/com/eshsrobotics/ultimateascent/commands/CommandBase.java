1f5759715c7fc0ee6af1246e271950fc4ddec6dc
Zachary Latta <zchlatta@gmail.com>
2013-02-17 08:59:42 -0800

    Removed useless subsystem.

--- src/com/eshsrobotics/ultimateascent/commands/CommandBase.java
+++ src/com/eshsrobotics/ultimateascent/commands/CommandBase.java
@@ -1,13 +1,10 @@
 package com.eshsrobotics.ultimateascent.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.eshsrobotics.ultimateascent.OI;
 import com.eshsrobotics.ultimateascent.RobotMap;
 import com.eshsrobotics.ultimateascent.subsystems.Climber;
 import com.eshsrobotics.ultimateascent.subsystems.DriveTrain;
-import com.eshsrobotics.ultimateascent.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj.Gyro;
 
 /**
  * The base for all commands. All atomic commands should subclass CommandBase.
@@ -20,8 +17,6 @@ public abstract class CommandBase extends Command
 {
 
     public static OI oi;
-    // Create a single static instance of all of your subsystems
-    public static ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
     public static DriveTrain chassis = new DriveTrain(RobotMap.leftDriveMotor,
             RobotMap.rightDriveMotor, RobotMap.driveG);
     public static Climber climber = new Climber(RobotMap.leftClimbMotor,
@@ -37,10 +32,6 @@ public abstract class CommandBase extends Command
         // yet. Thus, their requires() statements may grab null pointers. Bad
         // news. Don't move it.
         oi = new OI();
-
-
-        // Show what command your subsystem is running on the SmartDashboard
-        SmartDashboard.putData(exampleSubsystem);
     }
 
     public CommandBase(String name)
