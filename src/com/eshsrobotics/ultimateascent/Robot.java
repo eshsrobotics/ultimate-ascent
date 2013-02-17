1f8b826d0e2d6daec5f7bda20cb2fc0484d31ddd
Zachary Latta <zchlatta@gmail.com>
2013-02-16 18:43:55 -0800

    Removed extraneous class.

--- src/com/eshsrobotics/ultimateascent/Robot.java
+++ src/com/eshsrobotics/ultimateascent/Robot.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import com.eshsrobotics.ultimateascent.commands.CommandBase;
-import com.eshsrobotics.ultimateascent.commands.ExampleCommand;
 
 /**
  * The VM is configured to automatically run this class, and to call the
